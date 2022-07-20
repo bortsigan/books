3d161a10daec459122938507a0bc66d61f1104d3
Burt Karl Cabigas <bortsigan@users.noreply.github.com>
2022-07-20 21:49:18 +0800

    Update BookResource.php

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -5,9 +5,9 @@ declare (strict_types=1);
 
 namespace App\Http\Resources;
 
-
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Author;
+
 class BookResource extends JsonResource
 {
     /**
